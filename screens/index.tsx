f1346e4c836db94a112dd300bab7611868e7d2ca
jak-weston <jak.w.weston@gmail.com>
2024-07-02 14:38:38 -0400

    WIP index.js header

--- screens/index.tsx
+++ screens/index.tsx
@@ -1,3 +1,13 @@
+/*
+File Overview: This file serves as the home page for the app.
+Contributor: SD, JW
+Date of Creation: 07/02/2024
+Dependencies: None
+File Resources: None currently
+File Status: Work In Progress
+Bugs: None known
+*/
+
 import { Text, View } from "react-native";
 
 export default function Index() {
